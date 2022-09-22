7f0fc14111517286458511533811a9d4bafded5f
manishmahi <manishmahi1216@gmail.com>
2022-09-22 09:48:07 +0530

    modified code

--- src/com/problems/Tree5IdenticalTrees.java
+++ src/com/problems/Tree5IdenticalTrees.java
@@ -4,8 +4,11 @@ public class Tree5IdenticalTrees {
 	
 	private static boolean isIdentical(TreeNode root1, TreeNode root2) {
 		
-		if(root1==null || root2==null)
-			return root1==root2;
+		if(root1==null && root2==null)
+			return true;
+		
+		if((root1==null && root2!=null) || (root2==null && root1!=null))
+			return false;
 		
 		return ((root1.data == root2.data) && isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right));
 	}
